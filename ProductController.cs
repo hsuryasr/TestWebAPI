a8df14b7880f194b372e37d7a3f40f86b30f9ee9
hsuryasr <115404699+hsuryasr@users.noreply.github.com>
2022-12-17 12:11:17 +0000

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Entities db = new Entities();
+
+        // Add Post Request
+        public string Post(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return "Product Added";
+
+        }
+    }
+}
